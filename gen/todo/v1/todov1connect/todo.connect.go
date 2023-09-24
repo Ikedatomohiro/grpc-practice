91fc23a8107bb87373b6f3fb163ab5a671738b80
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-09-24 23:29:09 +0900

    createを実装

--- gen/todo/v1/todov1connect/todo.connect.go
+++ gen/todo/v1/todov1connect/todo.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "example.com/todo/v1"
+	v1 "example.com/todo/gen/todo/v1"
 	http "net/http"
 	strings "strings"
 )
